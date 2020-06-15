930d728677bd61daa7aaefc319f7ecd19ef1b654
bilalao <46230516+bilalao@users.noreply.github.com>
2020-06-15 15:42:54 +0100

    added listner and package in appcontext and main modified

--- src/ma/pfe/projet/test/Test.java
+++ src/ma/pfe/projet/test/Test.java
@@ -1,33 +1,30 @@
 package ma.pfe.projet.test;
 
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import ma.pfe.projet.dao.DaoBus;
 import ma.pfe.projet.dao.IDAO;
 import ma.pfe.projet.entity.Bus;
-
-
-import ma.pfe.projet.metier.IMetier;
-import ma.pfe.projet.metier.MetierBus;
 
 
 
 public class Test {
 
+	
 	public static void main(String[] args) {
+		IDAO<Bus> dao= new DaoBus();
+		// create a Bus
+				Bus b = new Bus(790,"Y-349", "Bus B", "Class C");
+				System.out.println("Saving the Bus");
+				System.out.println(b);
 
-		
-		IMetier<Bus> metier = new MetierBus();
-		
-		
-	    	ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-			
-			IDAO dao = (IDAO)ctx.getBean("daoBus");
-			
-	 		System.out.println(dao.findAll().size());
+				dao.save(b);
 		
 
 		System.out.println("Done!");
+		
+		
 
 	}
 
