cfc9b7e9612d3f19d4c1984f22505f28918d59e5
bilalao <46230516+bilalao@users.noreply.github.com>
2020-06-16 15:38:43 +0100

    updated

--- src/ma/pfe/projet/beans/BeanStation.java
+++ src/ma/pfe/projet/beans/BeanStation.java
@@ -1,11 +1,23 @@
 package ma.pfe.projet.beans;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import ma.pfe.projet.entity.Station;
+import ma.pfe.projet.metier.IMetier;
 
 @ManagedBean
 @Component
 public class BeanStation {
+	@Autowired
+	IMetier<Station> metierStation;
+	public List<Station> getListStation(){
+		return metierStation.findAll();
+	}
+	
 
 }
